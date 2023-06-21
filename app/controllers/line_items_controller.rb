2a5bf404f35c889cda849ec843b13aa18be03603
Djo-75 <j.pecquet@live.fr>
2023-06-21 14:42:16 +0200

    back cart okay!

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,51 @@
+class LineItemsController < ApplicationController
+    def create
+        # Find associated item and current cart
+        chosen_item = Item.find(params[:item_id])
+        current_cart = @current_cart
+      
+        # If cart already has this item then find the relevant line_item and iterate quantity otherwise create a new line_item for this item
+        if current_cart.items.include?(chosen_item)
+          # Find the line_item with the chosen_item
+          @line_item = current_cart.line_items.find_by(:item_id => chosen_item)
+          # Iterate the line_item's quantity by one
+          @line_item.quantity += 1
+        else
+          @line_item = LineItem.new
+          @line_item.cart = current_cart
+          @line_item.item = chosen_item
+        end
+      
+        # Save and redirect to cart show path
+        @line_item.save
+        redirect_to cart_path(current_cart)
+    end
+
+    def destroy
+        @line_item = LineItem.find(params[:id])
+        @line_item.destroy
+        redirect_to cart_path(@current_cart)
+    end  
+
+    def add_quantity
+        @line_item = LineItem.find(params[:id])
+        @line_item.quantity += 1
+        @line_item.save
+        redirect_to cart_path(@current_cart)
+    end
+      
+    def reduce_quantity
+        @line_item = LineItem.find(params[:id])
+        if @line_item.quantity > 1
+          @line_item.quantity -= 1
+        end
+        @line_item.save
+        redirect_to cart_path(@current_cart)
+    end
+      
+    private
+
+    def line_item_params
+        params.require(:line_item).permit(:quantity,:item_id, :cart_id)
+    end
+end
