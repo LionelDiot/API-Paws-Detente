044f97513273c88cb4a23be056df116c2d20d8b2
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-30 15:53:44 +0200

    attation requete commentée et fichier devise.rb commenté

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,26 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "the signup process", type: :request do
-  it "returns a 422 status code for an invalid sign-in attempt" do
-    headers = { 'Content-Type' => 'application/json' }
-    params = { user: { email: 'test@example.com', password: 'wrongpassword' } }.to_json
+# RSpec.describe "the register process", type: :request do
+#   it "returns a 200 status code for a valid sign-in attempt" do
+#     headers = { 'Content-Type' => 'application/json' }
+#     params = { user: { email: 'test@example.com', password: 'wrongpassword' } }.to_json
 
-    post '/users/sign_in', headers: headers, params: params
+#     post '/users/', headers: headers, params: params
 
-    expect(response).to have_http_status(422)
-  end
-end
+#     expect(response).to have_http_status(200)
+#   end
+# end
 
-RSpec.describe "the signup process", type: :request do
-  it "returns a 200 status code for a valid sign-in attempt" do
-    headers = { 'Content-Type' => 'application/json' }
-    params = { user: { email: 'test@example.com', password: '12345678' } }.to_json
 
-    post '/users/sign_in', headers: headers, params: params
 
-    expect(response).to have_http_status(200)
-  end
-end
 
 
 
