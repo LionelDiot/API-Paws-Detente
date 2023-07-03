a8b55bd75bf6c84de22f83eb60dbd342b9574418
Lionel Diot <diot.lionel2@gmail.com>
2023-07-03 12:29:54 +0200

    on remets l'etoile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "https://paws-detente.vercel.app/"
+     origins "*"
 
     resource '*',
       headers: %w[Authorization],
